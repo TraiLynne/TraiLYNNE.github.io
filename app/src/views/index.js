66746179e5d35c484c181b113bb7623fee4909c8
Trai Lynne Compton <trailynne@gmail.com>
2024-08-24 15:50:25 -0600

    Added view placeholders

---
+++ app/src/views/index.js
@@ -0,0 +1,8 @@
+import About from "./About";
+import Contact from "./Contact";
+import Footer from "./Footer";
+import Intro from "./Intro";
+import Nav from "./Nav";
+import Portfolio from "./Portfolio";
+
+export { About, Contact, Footer, Intro, Nav, Portfolio };
